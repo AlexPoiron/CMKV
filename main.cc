0fc0982458625510bbf1e5cd9d731e12ddb8f246
Guillaume Carriere <guillaume.carriere@epita.fr>
2023-01-18 17:42:58 +0100

    on avait pas la meme que bench

--- main.cc
+++ main.cc
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
         throw std::invalid_argument("Your command line has to be : command line ./a.out in.txt out.txt\n");
     }
 
-    Solver solver(0.8, 10, 0.9999);
+    Solver solver(0.8, 15, 0.9999);
 
     std::fstream infile(argv[1]);
     std::fstream outfile(argv[2], std::fstream::out);
